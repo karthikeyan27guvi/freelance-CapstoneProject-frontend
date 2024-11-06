12b1ef2e9fbcbc78c479a2e20343c14a43665264
karthikeyan27guvi <karthikeyan.github@gmail.com>
2024-11-06 18:41:27 +0530

    save changes

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Creating an Axios instance with a custom configuration
 const newRequest = axios.create({
-    baseURL:"https://freelance-capstoneproject-backend.onrender.com/api/", // Setting the base URL for API requests
+    baseURL:"http://localhost:8800/api/", // Setting the base URL for API requests
     withCredentials: true,  // Enabling the inclusion of credentials (like cookies) in cross-site requests
 });
 
