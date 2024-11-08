d60b80796753eb4d7182aef89965c1e3a2deed11
karthikeyan27guvi <karthikeyan.github@gmail.com>
2024-11-08 09:43:45 +0530

    save changes

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://freelance-capstoneproject-backend.onrender.com',
-        changeOrigin: true,
-        secure: true, // Set this to true if your backend uses HTTPS
-      },
-    },
-  },
 })
