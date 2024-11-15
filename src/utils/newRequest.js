d11904782e44b7458d95b3a4ec7eabb62de5a064
karthikeyan27guvi <karthikeyan.github@gmail.com>
2024-11-15 16:37:26 +0530

    save changes

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -19,7 +19,8 @@ newRequest.interceptors.request.use(
     (config) => {
         // Retrieve the token from localStorage
         const token = localStorage.getItem("accessToken");
-
+        console.log(token);
+        
         // If token is available, add it to the headers
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
